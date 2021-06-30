73bbb8ad99a71832deac79d34e3115d95f5e007b
Anshuman Suri <anshuman@email.virginia.edu>
2021-06-30 09:19:11 -0400

    Functionality-preserving changes

--- botgen/background.py
+++ botgen/background.py
@@ -1,16 +1,13 @@
 import sys, os
 import pandas as pd
-import subprocess
-from multiprocessing import Pool
 import datetime
 import csv
 import random
 import h5py
 import numpy as np
 import ipaddress
-import pathlib
 import datetime as datatime
-from itertools import product
+
 
 def write_single_graph(f, graph_id, x, edge_index, y, attrs=None, **kwargs):
     '''
@@ -25,6 +22,7 @@ def write_single_graph(f, graph_id, x, edge_index, y, attrs=None, **kwargs):
         for key in attrs:
             f[f'{graph_id}'].attrs[key] = attrs[key]
     return None
+
 
 def ip2int(ip):
     '''
@@ -46,22 +44,20 @@ def search_dict(IP, IP_dict):
         IP_dict[IP] = len(IP_dict)
     return IP_dict[IP]
 
-def prepare_background(f, dst_dir, dst_name, graph_id, start_time, stop_time):
-    '''
-    Transform txt files into standard hdf5 format
-    arg = [txt_file_name, subgroup of graphs]
-    '''
 
+def prepare_background_(f, start_time, stop_time):
     #read data
     df = pd.read_csv(f, sep = '@')#, nrows = 10000)#
     df.columns = ["time", "srcIP", "dstIP"]
 
+    # time-filtering not needed, since each file anyway
+    # contains per-minute logs
     #filter time
-    start_time_formated = datetime.datetime.strptime(start_time, "%Y%m%d%H%M%S")
-    stop_time_formated = datetime.datetime.strptime(stop_time, "%Y%m%d%H%M%S")
+    # start_time_formated = datetime.datetime.strptime(start_time, "%Y%m%d%H%M%S")
+    # stop_time_formated = datetime.datetime.strptime(stop_time, "%Y%m%d%H%M%S")
     df['time'] = df['time'].apply(lambda x: datetime.datetime.strptime(x[:21], "%b %d, %Y %H:%M:%S"))
-    df = df[ df.time >= start_time_formated]
-    df = df[ df.time < stop_time_formated]
+    # df = df[ df.time >= start_time_formated]
+    # df = df[ df.time < stop_time_formated]
 
     #transform time and IP address into formal type
     df["srcIP"] = df["srcIP"].apply(ip2int)
@@ -80,15 +76,34 @@ def prepare_background(f, dst_dir, dst_name, graph_id, start_time, stop_time):
     #write into h5py files
     num_nodes = len(IP_dict)
     num_edges = df.shape[0]
+
+    # # temp: print info
+    # print("%d nodes | %d edges" % (num_nodes, num_edges))
+
+    edge_index = np.array(df[["srcIP", "dstIP"]]).T
+
+    return edge_index, num_nodes, num_edges
+
+
+def prepare_background(f, dst_dir, dst_name, graph_id, start_time, stop_time):
+    '''
+    Transform txt files into standard hdf5 format
+    arg = [txt_file_name, subgroup of graphs]
+    '''
+
+    edge_index, num_nodes, num_edges = prepare_background_(f, start_time, stop_time)
     
     f_h5py = h5py.File(os.path.join(dst_dir,dst_name), 'a')
     write_single_graph(f_h5py, 
                         graph_id = graph_id, 
                         x = np.ones([num_nodes, 1]), 
-                        edge_index = np.array(df[["srcIP", "dstIP"]]).T,
+                        edge_index = edge_index,
                         y = np.zeros(num_nodes), 
                         attrs={'num_nodes': num_nodes, 'num_edges': num_edges, 'num_evils':0})
     f_h5py.close()
 
+
 if __name__ == '__main__':
-    prepare_background('equinix-nyc.dirA.20181220-131256.UTC.anon.pcap', '.', 'tmp.hdf5', 0, '20181220081256', '20181220081257')
+    # prepare_background('equinix-nyc.dirA.20181220-131256.UTC.anon.pcap', '.', 'tmp.hdf5', 0, '20181220081256', '20181220081257')
+    prepare_background('/p/adversarialml/as9rw/datasets/raw_botnet/temp.tmp',
+                       '/p/adversarialml/as9rw/datasets/raw_botnet', 'tmp.hdf5', 0, None, None)
--- botgen/download_pcap.py
+++ botgen/download_pcap.py
@@ -2,9 +2,8 @@
 This file download file from CAIDA to the same folder where the .py is
 python3 download_pcap.py
 '''
-import sys,os
-import subprocess
-from multiprocessing import Pool
+import os
+
 
 def download_and_decompress(raw_link, CAIDAUser, CAIDAPassword):
     '''
@@ -13,8 +12,6 @@ def download_and_decompress(raw_link, CAIDAUser, CAIDAPassword):
     - Output: pcap files. Example: equinix-nyc.dirA.20181115-130000.UTC.anon.pcap
     '''
     pcap_file = raw_link.split('/')[-1]
-    pcap_file_decompressed = pcap_file[:-3]
-    
     tmp_file = pcap_file + '.tmp'
 
     #download the file and name it.tmp
@@ -23,17 +20,29 @@ def download_and_decompress(raw_link, CAIDAUser, CAIDAPassword):
 
     # Rename file name when download is complete
     os.rename(tmp_file, pcap_file)
+
+    # de-comporess and process file
+    return decompress(pcap_file)
+
+
+def decompress(pcap_file, basepath=None):
+    pcap_file_decompressed = pcap_file[:-3]
+    tmp_file = pcap_file + '.tmp'
     
+    #move to directory, if requested
+    if basepath is not None:
+        os.chdir(basepath)
+
     #decompress the data
     cmd = f'gunzip {pcap_file}'
     os.system(cmd)   
     
     #read time, src, dst
     cmd = f'tshark -r {pcap_file_decompressed} -Y \"ip\" -T fields -E separator=@ -e frame.time -e ip.src -e ip.dst > {tmp_file}'
+    os.system(cmd)
 
     # Rename file name when reading is complete
     cmd = f'mv {tmp_file} {pcap_file_decompressed}'
+    os.system(cmd)
 
     return pcap_file_decompressed
-
-
